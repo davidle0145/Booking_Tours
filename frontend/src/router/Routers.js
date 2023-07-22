3095a4979f19a367a14329647fb4dd6679629030
LE THANH DAT <ledat10b8@gmail.com>
2023-07-22 20:21:50 +0700

    first commit

---
+++ frontend/src/router/Routers.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+import About from './../pages/About'
+import Home from './../pages/Home'
+import Login from './../pages/Login'
+import Register from './../pages/Register'
+import SearchResultList from './../pages/SearchResultList'
+import TourDetails from './../pages/TourDetails'
+import Tours from './../pages/Tours'
+
+const Routers = () => {
+  return (
+    <Routes>
+        <Route path = '/' element = {<Navigate to = '/home' />} />
+        <Route path = '/about' element = {<About/>} />
+        <Route path = '/home' element = {<Home/>} />
+        <Route path = '/login' element = {<Login/>} />
+        <Route path = '/register' element = {<Register/>} />
+        <Route path = '/tours/search' element = {<SearchResultList/>} />
+        <Route path = '/tours/:id' element = {<TourDetails/>} />
+        <Route path = '/tours' element = {<Tours/>} />
+    </Routes>
+  )
+}
+
+export default Routers
