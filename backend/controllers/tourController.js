e0e50a8a12c50f280cce3373daaf5b95ef90f0f8
LE THANH DAT <ledat10b8@gmail.com>
2023-07-26 18:51:55 +0700

    crud tour api

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -6,13 +6,147 @@ export const createTour = async (req, res) => {
         const savedTour = await newTour.save()
         res.status(200).json({
             success: true,
-            message: "Successfully created",
+            message: "Successfully created Tour",
             data: savedTour
         })
     } catch (err) {
         res.status(500).json({
             success: false,
-            message: "Failed to create. Try again"
+            message: "Failed to create Tour. Try again"
         })
     }
 }
+
+export const updateTour = async(req, res) => {
+    const id = req.params.id
+    try {
+        const updateTour = await Tour.findByIdAndUpdate(id, {
+            $set: req.body
+        }, {new:true})
+
+        res.status(200).json({
+            success: true,
+            message: "Successfully updated Tour",
+            data: updateTour
+        })
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to update Tour. Try again"
+        })
+    }
+}
+
+export const deleteTour = async(req, res) => {
+    const id = req.params.id
+    try {
+        await Tour.findByIdAndDelete(id)
+        res.status(200).json({
+            success: true,
+            message: "Successfully deleted Tour",
+        })
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to delete Tour. Try again"
+        })
+    }
+}
+
+export const getTourByID = async(req, res) => {
+    const id = req.params.id
+    try {
+        const tour = await Tour.findById(id)
+        res.status(200).json({
+            success: true,
+            message: `Successfully get Tour: ${tour.title}`,
+            data: tour
+        })
+    } catch (err) {
+        res.status(404).json({
+            success: false,
+            message: "Tour not found!"
+        })
+    }
+}
+
+export const getAllTour = async(req, res) => {
+
+    // pagination
+    const page = parseInt(req.query.page)
+    try {
+        const tours = await Tour.find({}).skip(page*8).limit(8)
+        res.status(200).json({
+            success: true,
+            count: tours.length,
+            message: "Successfully get All Tour:",
+            data: tours
+        })
+    } catch (err) {
+        res.status(404).json({
+            success: false,
+            message: "Error"
+        })
+    }
+}
+
+export const getTourBySearch = async(req, res) => {
+    
+    const city = new RegExp(req.query.city, 'i')
+    const distance = parseInt(req.query.distance)
+    const maxGroupSize = parseInt(req.query.maxGroupSize)
+
+    try {
+        const tours = await Tour.find({
+            city,
+            distance: {$gte:distance},
+            maxGroupSize: {$gte:maxGroupSize}
+        })
+
+        res.status(200).json({
+            success: true,
+            message: "Successfully get Tour by Search",
+            data: tours
+        })
+
+    } catch (err) {
+        res.status(404).json({
+            success: false,
+            message: "Error"
+        })
+    }
+}
+
+export const getFeaturedTour = async(req, res) => {
+
+    try {
+        const tours = await Tour.find({featured:true}).limit(8)
+        res.status(200).json({
+            success: true,
+            message: "Successfully get Featured Tour:",
+            data: tours
+        })
+    } catch (err) {
+        res.status(404).json({
+            success: false,
+            message: "Error"
+        })
+    }
+}
+
+export const getTourCount = async(req, res) => {
+
+    try {
+        const tourCount = await Tour.estimatedDocumentCount()
+        res.status(200).json({
+            success: true,
+            data: tourCount
+        })
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to fetch"
+        })
+    }
+}
+
--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -1,10 +1,15 @@
 import express from 'express'
-import { createTour } from '../controllers/tourController.js'
+import { createTour, updateTour, deleteTour, getTourByID, getAllTour, getTourBySearch, getFeaturedTour, getTourCount } from '../controllers/tourController.js'
 
 const router = express.Router()
 
 router.post('/', createTour)
-
-
+router.put('/:id', updateTour)
+router.delete('/:id', deleteTour)
+router.get('/:id', getTourByID)
+router.get('/', getAllTour)
+router.get('/search/getTourBySearch', getTourBySearch)
+router.get('/search/getFeaturedTours', getFeaturedTour)
+router.get('/search/getTourCount', getTourCount)
 
 export default router
